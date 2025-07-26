c86d259b63be8c85427afc488a2a1727352a2889
ortab123 <131905006+ortab123@users.noreply.github.com>
2025-07-26 12:10:48 +0300

    Step 4 finalized

--- main.js
+++ main.js
@@ -1,5 +1,54 @@
 const tweeter = Tweeter();
 const renderer = Renderer();
 
-// This should render the initial dummy data
 renderer.renderPosts(tweeter.getPosts());
+
+$("#twit-button").on("click", function () {
+  console.log("Twit button clicked!");
+
+  const inputText = $("#new-post-input").val().trim();
+
+  if (inputText) {
+    tweeter.addPost(inputText);
+    renderer.renderPosts(tweeter.getPosts());
+    $("#new-post-input").val("");
+  }
+});
+
+$(document).on("click", ".comment-button", function () {
+  console.log("Comment button clicked!");
+
+  const postElement = $(this).closest(".post");
+  const postId = postElement.data("id");
+  const commentText = postElement.find(".comment-input").val().trim();
+
+  if (commentText) {
+    tweeter.addComment(postId, commentText);
+    renderer.renderPosts(tweeter.getPosts());
+  }
+});
+
+$(document).on("click", ".delete-comment", function () {
+  console.log("Delete comment button clicked!");
+
+  const postElement = $(this).closest(".post");
+  const postId = postElement.data("id");
+  const commentId = $(this).data("id");
+
+  if (commentId && postId) {
+    tweeter.removeComment(postId, commentId);
+    renderer.renderPosts(tweeter.getPosts());
+  }
+});
+
+$(document).on("click", ".delete", function () {
+  console.log("Delete post button clicked!");
+
+  const postElement = $(this).closest(".post");
+  const postId = postElement.data("id");
+
+  if (postId) {
+    tweeter.removePost(postId);
+    renderer.renderPosts(tweeter.getPosts());
+  }
+});
--- render.js
+++ render.js
@@ -9,28 +9,30 @@ const Renderer = function () {
   };
 
   const createCommentElement = function (comment) {
-    return $(`
-        <div class="comment" data-id="${comment.id}">
-        ${comment.text}
-        <div class="delete-comment" data-id="${comment.id}">X</div>
-        </div>
-        `);
+    const commentEl = $(`
+    <div class="comment" data-id="${comment.id}">
+      <span class="delete-comment" data-id="${comment.id}">x</span>
+      <span class="comment-text">${comment.text}</span>
+    </div>
+  `);
+    return commentEl;
   };
 
   const createPostElement = function (post) {
     const postEl = $(`
         <div class="post" data-id="${post.id}">
         <div class="post-text">${post.text}</div>
-        <div class="delete" data-id="${post.id}">Delete Post</div>
         <div class="comments"></div>
         <input type="text" placeholder="Got something to say?" class="comment-input">
         <button class="comment-button">Comment</button>
+        <button class="delete" data-id="${post.id}">Delete Post</button>
         </div>
         `);
 
     const commentContainer = postEl.find(".comments");
     for (let comment of post.comments) {
-      commentContainer.append(createCommentElement(comment));
+      const commentEl = createCommentElement(comment);
+      commentContainer.append(commentEl);
     }
 
     return postEl;
